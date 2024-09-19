d076693481f5ef07c866c08f4cb101895b2d0903
Garret Beebe <garret.beebe@gmail.com>
2024-09-19 07:32:40 -0700

    fix versioning issues with flask and werkzeug

--- pig-latin-flask/main.py
+++ pig-latin-flask/main.py
@@ -60,4 +60,4 @@ def web_interface():
     return render_template_string(html, result=result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0',port=8000,debug=True)
